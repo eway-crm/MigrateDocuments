43aa7ca4d6ffd44266551756e79f0891ece48d89
Jan Ligač <jan.ligac@eway-crm.com>
2024-09-23 11:23:25 +0200

    refs #47766: Move project to Sdk and cleanup usings

--- Config.cs
+++ Config.cs
@@ -1,10 +1,5 @@
 ﻿using MigrateDocuments.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MigrateDocuments
 {
